7b36d96eb55fa8990ee47e756853ffcb54382f93
student <student@240-13.INFORMATYKA.local>
2016-10-06 14:55:15 +0200

    zadanie 4 - problem nie rozwiazany

--- src/org/yourorghere/Wiacek.java
+++ src/org/yourorghere/Wiacek.java
@@ -49,81 +49,55 @@ public class Wiacek implements GLEventListener {
         frame.setVisible(true);
         animator.start();
     }
+ 
+     public void DisplayTriangleFan(float r, float xsr, float ysr, GL gl) {
+ 
+         float x, y, kat;
+         gl.glBegin(GL.GL_TRIANGLE_FAN);
+         gl.glVertex3f(xsr,ysr,-6.0f); //?rodek
+         for(kat = 0.0f; kat < (2.0f*Math.PI);
+         kat+=(Math.PI/32.0f))
+         {
+         x = r*(float)Math.sin(kat)+xsr;
+         y = r*(float)Math.cos(kat)+ysr;
+         gl.glVertex3f(x, y, -6.0f); //kolejne punkty
+         gl.glVertex3f(xsr, ysr, -6.0f); //?rodek
+         for (kat = 0.0f; kat < (2.0f * Math.PI);
+                 kat += (Math.PI / 32.0f)) {
+             x = r * (float) Math.sin(kat) + xsr;
+             y = r * (float) Math.cos(kat) + ysr;
+             gl.glVertex3f(x, y, -6.0f); //kolejne punkty
+          }
+          gl.glEnd();
+         
+ 
+      }
+ 
+      public void display(GLAutoDrawable drawable) {
+          GL gl = drawable.getGL();
+  
+          // Clear the drawing area
+          gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+          // Reset the current matrix to the "identity"
+          gl.glLoadIdentity();
+                 
+         DisplayTriangleFan(1.0f , 0.0f , 0.0f, gl);
+      
+ 
+         DisplayTriangleFan(1.0f, 1.0f, 1.0f, gl);
+ 
+          // Flush all drawing operations to the graphics card
+          gl.glFlush();
+      }
+  
+      public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+      }
 
     public void init(GLAutoDrawable drawable) {
-        // Use debug pipeline
-        // drawable.setGL(new DebugGL(drawable.getGL()));
-
-        GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
-
-        // Enable VSync
-        gl.setSwapInterval(1);
-
-        // Setup the drawing area and shading mode
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        GL gl = drawable.getGL();
-        GLU glu = new GLU();
-
-        if (height <= 0) { // avoid a divide by zero error!
-        
-            height = 1;
-        }
-        final float h = (float) width / (float) height;
-        gl.glViewport(0, 0, width, height);
-        gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
-        glu.gluPerspective(45.0f, h, 1.0, 20.0);
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    public void display(GLAutoDrawable drawable) {
-        //Tworzenie obiektu
-        GL gl = drawable.getGL();
-        GL g2 = drawable.getGL();
-        GL g3 = drawable.getGL();
-        //Czyszczenie przestrzeni 3D przed utworzeniem kolejnej klatki
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-        //Resetowanie macierzy transformacji
-        gl.glLoadIdentity();
- 
-    gl.glColor3f(0.0f,0.0f,3.0f);
-   
-         //Wykonanie wszystkich operacji znajduj¹cych siê w buforze
-        gl.glFlush();
-        gl.glBegin(GL.GL_TRIANGLES);
-
-        gl.glVertex3f(-1.0f, 1.2f, -10.0f);
-        gl.glVertex3f(-2.0f,-1.2f, -10.0f);
-        gl.glVertex3f( 0.0f,-1.2f, -10.0f);
-        gl.glEnd();
-        
-              g2.glFlush();
-        g2.glBegin(GL.GL_QUADS);
-        g2.glColor3f( 5.0f,2.0f,1.0f);
-        g2.glVertex3f( -2.0f, -1.2f, -10.0f);
-        g2.glVertex3f(-0.0f, -1.2f, -10.0f);
-        g2.glVertex3f( -0f, -3.0f, -10.0f);
-        g2.glVertex3f( -2.0f, -3.0f, -10.0f);
-        g2.glEnd();
-        
-                   g3.glFlush();
-        g3.glBegin(GL.GL_QUADS);
-          g3.glColor3f(0.0f,0.0f,3.0f);
-        g3.glVertex3f( -1.0f, -2.5f, -10.0f);
-        g3.glVertex3f(-1.0f, -3.0f, -10.0f);
-        g3.glVertex3f( -0.5f, -3.0f, -10.0f);
-        g3.glVertex3f( -0.5f, -2.5f, -10.0f);
-        g3.glEnd();
-
-    }
-
-    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
-    }
-}
-
+  }
